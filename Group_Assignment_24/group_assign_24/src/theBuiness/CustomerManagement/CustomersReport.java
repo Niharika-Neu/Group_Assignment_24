a584968a9ba7458394d62fb350716bc143d38619
MettukuruMrudul <mettukuru.m@northeastern.edu>
2025-11-16 21:24:59 -0500

    Customermodelm2

---
+++ Group_Assignment_24/group_assign_24/src/theBuiness/CustomerManagement/CustomersReport.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theBuiness.CustomerManagement;
+
+import theBuiness.CustomerManagement.CustomerSummary;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class CustomersReport {
+    ArrayList<CustomerSummary> customerlist;
+    
+    public CustomersReport(){
+        
+    }
+    public void addCustomerSummary(CustomerSummary cs){
+        
+    }
+}
